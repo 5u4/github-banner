3450daea69fc43cce886a6708e64d3edaa7050eb
Senhung <alexwongsenhung@gmail.com>
2019-11-10 15:59:31 -0800

    Add markdown tag

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,8 @@
   let fontSize = 32;
   let fontFamily = "'Varela Round'";
 
+  const code = `<p align="center"><img src="./assets/banner.png" /></p>`;
+
   const produce = () => {
     const ctx = canvasRef.getContext("2d");
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -30,12 +32,54 @@
   canvas {
     margin: 30px auto;
     display: block;
-    outline: #aaa 3px solid;
+    /* outline: #aaa 3px solid; */
     font-family: "Varela Round", sans-serif;
+    -moz-box-shadow: 0 0 3px 6px #dddddd94;
+    -webkit-box-shadow: 0 0 3px 6px #dddddd94;
+    box-shadow: 0 0 3px 6px #dddddd94;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 8px;
+    border-color: #aaa;
   }
   input {
-    margin: 30px auto;
     display: block;
+    margin: 30px auto;
+    font-size: 16px;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 8px;
+    border-color: #aaa;
+    -moz-box-shadow: 0 0 1px 3px #dddddd94;
+    -webkit-box-shadow: 0 0 1px 3px #dddddd94;
+    box-shadow: 0 0 1px 3px #dddddd94;
+    padding: 8px;
+    outline: none;
+    width: 350px;
+  }
+  input:hover {
+    border-color: #3686ff;
+  }
+  input:focus {
+    border-color: #3686ff;
+  }
+  pre {
+    margin: 30px auto;
+    background: #f4f4f4;
+    border: 1px solid #ddd;
+    border-left: 3px solid #3686ff;
+    color: #4d4d4d;
+    font-family: monospace;
+    font-size: 16px;
+    max-width: 530px;
+    padding: 10px 20px;
+    display: block;
+    -moz-box-shadow: 0 0 1px 3px #dddddd94;
+    -webkit-box-shadow: 0 0 1px 3px #dddddd94;
+    box-shadow: 0 0 1px 3px #dddddd94;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 8px;
   }
 </style>
 
@@ -43,3 +87,6 @@
 <input
   bind:value={projectName}
   on:keypress={e => e.code === 'Enter' && produce()} />
+<pre>
+  <code>{code}</code>
+</pre>
